968ce34920691f51f08e94648805b535d299772f
Mike Verdicchio <mikeverdicchio@gmail.com>
2019-06-16 22:17:27 -0400

    Add Heroku setup

--- toast_chest/heroku.py
+++ toast_chest/heroku.py
@@ -1,5 +1,9 @@
 from toast_chest.base import *
 import dj_database_url
+import django_heroku
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
