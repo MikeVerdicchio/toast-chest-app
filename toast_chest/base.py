ff08dca7c55824a6a2d6ab15db2951ea0343ecad
Mike Verdicchio <mikeverdicchio@gmail.com>
2019-10-30 20:33:08 -0900

    Add DRF renderer

--- toast_chest/base.py
+++ toast_chest/base.py
@@ -69,6 +69,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)
+}
+
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
