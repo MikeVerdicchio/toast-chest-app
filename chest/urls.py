efcbdf31c02b725ac8c51118676da4e8ec7558b3
Mike Verdicchio <mikeverdicchio@gmail.com>
2020-04-11 19:43:33 -0400

    Add health endpoint

--- chest/urls.py
+++ chest/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import GetRandomView, HomepageView
+from .views import GetRandomView, HealthCheck, HomepageView
 
 urlpatterns = [
     url(r"^$", HomepageView.as_view(), name="chest"),
+    url(r"^health$", HealthCheck.as_view(), name="health-check"),
     url(r"^random.json$", GetRandomView.as_view(), name="random-toast"),
 ]
